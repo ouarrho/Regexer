927dd9be75bb4f389a080045727d00c63c747aa5
oualid <34518781+ouarrho@users.noreply.github.com>
2023-01-20 12:01:56 +0100

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,55 @@
 console.log('hello')
+
+/**
+	*	Lexing
+	*	
+	* INSERT
+	* SELECT
+	* UPDATE
+	* DELETE
+	*
+	* AS
+	* 
+	* FROM
+	*
+	* WHERE
+	* 
+	* THIS
+	* NOT
+	* AND
+	* OR
+	* XOR
+	* IN
+	* AT
+	* OF
+	* LIKE
+	* 
+	* 
+	* BEGINNING
+	* END
+	* FOLLOWED
+	* BETWEEN
+	* ALL
+	* GROUP
+	* BY
+	* ORDER
+	* LIMIT
+	* LINE
+	* WORD
+	* 
+*/
+
+/**
+	* RegEx: /abc/
+	* Regexer: SELECT 'abc'
+	* RegEx: /^abc/
+	* Regexer: SELECT 'abc' AT BEGINNING
+	* RegEx: /abc$/
+	* Regexer: SELECT 'abc' AT END
+	* RegEx: /ab(?= c)/;
+	* Regexer: SELECT 'ab' FOLLOWED BY 'c'
+	* RegEx: /(?<=")(abc)(?=")/
+	* Regexer: SELECT 'abc' BETWEEN '"'
+	* RegEx: /abc/g
+	* Regexer: SELECT ALL 'abc'
+*/
